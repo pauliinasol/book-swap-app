1c039066cd7736bc9b7830dfeef766596a90bca2
Pauliina Solanne <pauliina.solanne@gmail.com>
2019-01-24 20:39:31 +0200

    server working

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-const UserPage = props => {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
-};
+class UserPage extends Component {
+  state = { users: [] };
 
+  componentDidMount() {
+    fetch("/users")
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+        {this.state.users.map(user => (
+          <div key={user.id}>{user.username}</div>
+        ))}
+      </div>
+    );
+  }
+}
 export default UserPage;
